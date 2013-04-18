aab4dd7eea5351c3274614121ecb7ccd8c165209
zhangkai <176776005@qq.com>
2013-04-18 11:10:34 +0800

    add the iphone hack project

---
+++ EFHack2013/EFHack2013/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EFHack2013
+//
+//  Created by zhangkai on 4/18/13.
+//  Copyright (c) 2013 zhangkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ EFHack2013/EFHack2013/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EFHack2013
+//
+//  Created by zhangkai on 4/18/13.
+//  Copyright (c) 2013 zhangkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
